36c62377d5ee406ff7bd7d7c6debafedd2f23419
kevinreber <kevinreber1@gmail.com>
2019-12-27 18:39:04 -0800

    Added modal and filter

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const pokedex = document.getElementById('pokedex');
 const filter = document.getElementById('filter');
+let cards;
+let pokeData;
+let pokeIndex;
 
 const fetchPokemon = () => {
     const promises = [];
     //loop through original 151 pokemon
     for (let i = 1; i <= 151; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
+        promises.push(fetch(url)
+            .then(res => res.json()));
     }
 
-    Promise.all(promises).then((results) => {
-        const pokeData = results.map((data) => ({
+    Promise.all(promises).then(results => {
+        pokeData = results.map((data) => ({
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
-            type: data.types.map((type) => type.type.name).join(', ')
+            shiny: data.sprites['front_shiny'],
+            type: data.types.map(type => type.type.name).join(', '),
+            abilities: data.abilities.map(ability => ability.ability.name).join(', ')
         }));
         displayPokemon(pokeData);
     });
 };
 
-const displayPokemon = (pokeData) => {
+function displayPokemon(pokeData) {
     //.join() to keep all cards in array[0]
     const pokemonHTMLString = pokeData.map(pokemon =>
         `<li class="card">
@@ -29,12 +35,88 @@ const displayPokemon = (pokeData) => {
             <p class="card-subtitle">Type: ${pokemon.type}</p> 
             </li>`).join('');
     pokedex.innerHTML = pokemonHTMLString;
-};
 
-fetchPokemon();
+    cards = document.querySelectorAll('.card');
+    Array.from(cards).forEach((card, index) => {
+        card.addEventListener('click', () => {
+            pokeIndex = index;
+            displayModal(pokeData[index]);
+        });
+    })
+}
 
-filter.addEventListener('keydown', (e) => {
-    let input = e.target.value;
+function displayModal(pokemon) {
+    const pokemonHTMLString = `
+    <div class="modal-overlay">
+        <div class="modal-cycle-container">
+            <div class="modal-button modal-prev">
+                <span> < </span>
+            </div>
+        </div>
+        <div class="modal-card">
+            <svg class="modal-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
+                        <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/> 
+            </svg>
+            <div class="modal-images">
+                <img class="card-image" src="${pokemon.image}" />
+                <img class="card-image" src="${pokemon.shiny}" />
+            </div>
+            <div class="modal-text">
+                <h2 class="card-title modal-title"> ${pokemon.id}.${pokemon.name} </h2> 
+                <p class="card-subtitle modal-text">Abilities: ${pokemon.abilities}</p>
+                <p class="card-subtitle modal-text">Type: ${pokemon.type}</p> 
+            </div> 
+        </div>
+        <div class="modal-cycle-container">
+            <div class="modal-button modal-next">
+                <span> > </span>
+            </div>
+        </div>
+    </div>
+    `
+    overlay.innerHTML = pokemonHTMLString;
+    overlay.style.display = 'block';
+}
 
-    console.log(input.length);
+function displayMatches() {
+    const matchArray = findMatches(this.value, pokeData);
+    displayPokemon(matchArray);
+}
+
+function findMatches(wordToMatch, data) {
+    const matches = data.filter(pokemon => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return pokemon.type.match(regex);
+    });
+    return matches;
+}
+
+overlay.addEventListener('click', (e) => {
+    const prev = overlay.querySelector('.modal-prev');
+    const next = overlay.querySelector('.modal-next');
+
+    if (e.target.classList.contains('modal-close') || e.target.parentElement.classList.contains('modal-close') || e.target.classList.contains('modal-overlay')) {
+        overlay.style.display = 'none';
+    }
+
+    if (e.target === next || e.target.parentElement === next) {
+        if (pokeIndex === 150) {
+            pokeIndex = 0;
+        } else {
+            pokeIndex++;
+        }
+        displayModal(pokeData[pokeIndex]);
+    }
+
+    if (e.target === prev || e.target.parentElement === prev) {
+        if (pokeIndex === 0) {
+            pokeIndex = 150;
+        } else {
+            pokeIndex--;
+        }
+        displayModal(pokeData[pokeIndex]);
+    }
 });
+
+filter.addEventListener('keyup', displayMatches);
+fetchPokemon();
