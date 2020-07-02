d4b583910355b81a4efefeb7b51c7bde737e681c
kevinreber <kevinreber1@gmail.com>
2020-07-02 15:05:31 -0700

    add: 'useAxios' hook to handle API request calls

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,58 +1,33 @@
 import React, {useState, useEffect} from 'react';
+import useAxios from '../hooks/useAxios';
 import axios from 'axios';
 import Card from './Card';
 
 const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 function Pokedex(){
-    const [pokemon, setPokemon] = useState([]);
+    const [pokemon, errors, isLoading] = useAxios(1,5);
     // const [modal, setModal] = useState(false);
 
-    /** get pokemon data from API */
-    useEffect(() =>{
-        async function getPokemon(){
-            try{
-                let results = [];
 
-                for (let i = 1; i <= 151; i++) {
-                    let resp = await axios.get(`${API_BASE_URL}/${i}`);
-                    results.push(resp.data);
-                }
-
-                results.map(r => {
-                        setPokemon(p => [...p,
-                        {
-                            name: r.name,
-                            id: r.id,
-                            image: r.sprites['front_default'],
-                            shiny: r.sprites['front_shiny'],
-                            type: r.types.map(type => type.type.name).join('/').toUpperCase(),
-                            abilities: r.abilities.map(ability => ability.ability.name).join(', ')
-                        }
-                ])
-            });
-            } catch (err){
-                alert(err);
-            }
-        }
-        
-        getPokemon();
-    })
 
     const pokemons = pokemon.map(p => (
         <Card
-            key = {p.id}
-            id = {p.id}
-            name = {p.name} 
-            image = {p.image}
-            type = {p.type}
+            // key = {p.id}
+            // id = {p.id}
+            // name = {p.name} 
+            // image = {p.image}
+            // type = {p.type}
+            {...p}
         />
     ));
 
     return (
         <div className="Pokedex">
         <ol id="pokedex">
-            { pokemon ? pokemons : (<h3>Loading...</h3>) }
+            { isLoading ? (<h3>Loading...</h3>) : pokemons }
+
+            {/* { pokemon ? pokemons : (<h3>Loading...</h3>) } */}
             {/* {pokemons} */}
         </ol>
         </div>
---
+++ src/hooks/useAxios.js
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+function useAxios(start=1,end=150){
+    const [responses, setResponse] = useState([]);
+    const [errors, setErrors] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    /** get pokemon data from API */
+    useEffect(() => {
+        const getData = async () =>{
+            try{
+                let results = [];
+    
+                for (let i = start; i <= end; i++) {
+                    let resp = await axios.get(`${BASE_URL}/${i}`);
+                    results.push(resp.data);
+                }
+        
+                results.map(r => {
+                        setResponse(p => [...p,
+                        {
+                            name: r.name,
+                            id: r.id,
+                            image: r.sprites['front_default'],
+                            shiny: r.sprites['front_shiny'],
+                            type: r.types.map(type => type.type.name).join('/').toUpperCase(),
+                            abilities: r.abilities.map(ability => ability.ability.name).join(', ')
+                        }
+                    ]);
+                });
+                setIsLoading(false);
+            } catch (err) {
+                setErrors(err);
+            }
+        }
+        getData();
+    });
+    return [responses, errors, isLoading];
+}
+
+export default useAxios;
