6daa71a1da69b07d505c14dfaeaf5c3637a210b6
kevinreber <kevinreber1@gmail.com>
2020-07-03 13:29:52 -0700

    fixed: typo in style for Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,7 @@ function Modal(props){
                         <span> &#60; </span>
                     </div>
                 </div>
-                <div classNames="modal-card">
+                <div className="modal-card">
                     <svg onClick={props.onHide} className="modal-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
                                 <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/> 
                     </svg>
