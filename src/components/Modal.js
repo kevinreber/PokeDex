840665a15f6722b5674da11cf98d052bc399d5f1
kevinreber <kevinreber1@gmail.com>
2020-07-02 23:20:23 -0700

    add: toggle Modal feature to display correct Pokemon

---
+++ src/components/Modal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Modal(props){
+    return (
+        <>
+            <div className="modal-overlay">
+                <div className="modal-cycle-container">
+                    <div className="modal-button modal-prev">
+                        <span> &#60; </span>
+                    </div>
+                </div>
+                <div classNames="modal-card">
+                    <svg onClick={props.onHide} className="modal-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
+                                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/> 
+                    </svg>
+                    <div className="modal-images">
+                        <img className="card-image" src={props.image} alt="{props.name}"/>
+                        <img className="card-image" src={props.shiny} alt="{props.name}"/>
+                    </div>
+                    <div className="modal-text">
+                        <h2 className="card-title modal-title"> {props.id}.{props.name} </h2> 
+                        <p className="card-subtitle modal-text">Type: {props.type}</p>
+                        <p className="card-subtitle modal-text">Abilities: {props.abilities}</p>
+                    </div> 
+                </div>
+                <div className="modal-cycle-container">
+                    <div className="modal-button modal-next">
+                        <span> &#62; </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Modal;
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import useAxios from '../hooks/useAxios';
 import Card from './Card';
+import Modal from './Modal';
 
 function Pokedex(){
     const [pokemon, errors, isLoading] = useAxios(1,5);
-    // const [modal, setModal] = useState(false);
+    const [modalShow, setModalShow] = useState(false);
+    const[pokeModal, setPokeModal] = useState(null);
+
+    /** Toggle's modal and get's card's 'id' for 'pokModal' */
+    const toggleModal = (e) => {
+        setModalShow( m => m = !m);
+
+        /** reset pokeModal */
+        if(modalShow){
+            setPokeModal(null);
+        } else {
+            /** set 'pokeModal' id to display in Modal */
+            const parent = e.target.closest('li');        
+            setPokeModal(pokemon[parent.id - 1]);
+        }
+    }
 
     /** return error screen if error has ocurred */
     if (errors){
-        return (<h3 id="Loading" >Sorry, an error has occured!</h3>)
+        return (<h3 id="Loading" >Sorry, an error has occurred!</h3>)
     }
 
+    /** build Card component for pokemon */
     const pokemons = pokemon.map(p => (
         <Card
-            // key = {p.id}
-            // id = {p.id}
-            // name = {p.name} 
-            // image = {p.image}
-            // type = {p.type}
-            {...p}
+            key = {p.id}
+            id = {p.id}
+            name = {p.name} 
+            image = {p.image}
+            type = {p.type}
+            toggleModal = {toggleModal}
         />
     ));
 
     return (
+        <>
+        <div id="overlay" 
+            className={modalShow ? "show" : "hide"}
+            onClick={toggleModal}>
+            <Modal {...pokeModal} />
+        </div>
         <div className="Pokedex">
         <ol id="pokedex">
             { isLoading ? (<h3 id="Loading" >Loading...</h3>) : pokemons }
         </ol>
         </div>
+        </>
     )
 }
 
