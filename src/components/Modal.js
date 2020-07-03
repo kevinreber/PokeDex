8e1cb3bbc33b2a3fbe267d4828b58a75963ace47
kevinreber <kevinreber1@gmail.com>
2020-07-03 14:27:21 -0700

    add: featured to navigate through Pokemon in Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,14 +3,14 @@ import React from 'react';
 function Modal(props){
     return (
         <>
-            <div className="modal-overlay">
+            <div onClick={props.toggleModal} className="modal-overlay">
                 <div className="modal-cycle-container">
-                    <div className="modal-button modal-prev">
-                        <span> &#60; </span>
+                    <div onClick={props.navigateModal} data-nav="prev" className="modal-button modal-prev">
+                        <span data-nav="prev"> &#60; </span>
                     </div>
                 </div>
                 <div className="modal-card">
-                    <svg onClick={props.onHide} className="modal-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
+                    <svg onClick={props.toggleModal} className="modal-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
                                 <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/> 
                     </svg>
                     <div className="modal-images">
@@ -24,8 +24,8 @@ function Modal(props){
                     </div> 
                 </div>
                 <div className="modal-cycle-container">
-                    <div className="modal-button modal-next">
-                        <span> &#62; </span>
+                    <div onClick={props.navigateModal} data-nav="next" className="modal-button modal-next">
+                        <span data-nav="next"> &#62; </span>
                     </div>
                 </div>
             </div>
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,22 +4,44 @@ import Card from './Card';
 import Modal from './Modal';
 
 function Pokedex(){
+    // State to store pokemon
     const [pokemon, errors, isLoading] = useAxios(1,5);
+    // State to display Modal
     const [modalShow, setModalShow] = useState(false);
-    const[pokeModal, setPokeModal] = useState(null);
+    // State to display which Pokemon on Modal
+    const [pokeModal, setPokeModal] = useState(null);
 
     /** Toggle's modal and get's card's 'id' for 'pokModal' */
     const toggleModal = (e) => {
-        setModalShow( m => m = !m);
 
-        /** reset pokeModal */
-        if(modalShow){
-            setPokeModal(null);
-        } else {
-            /** set 'pokeModal' id to display in Modal */
-            const parent = e.target.closest('li');        
-            setPokeModal(pokemon[parent.id - 1]);
+        /** Should only be able to toggle Modal if clicking outside of 
+         * Modal when Modal is open or when Modal is closed */
+        if(e.target.className === 'modal-overlay' || e.target.className === 'modal-close' || !modalShow){
+            setModalShow( m => m = !m);
+
+            /** reset pokeModal */
+            if(modalShow){
+                setPokeModal(null);
+            } else {
+                /** set 'pokeModal' id to display in Modal */
+                const parent = e.target.closest('li');        
+                setPokeModal(parent.id - 1);
+            }
         }
+    }
+
+    /** Define 'navigateModal' as a 'function' b/c using 'const' will give us errors when re-using 'navigateModal' */
+    function navigateModal(e) {
+        /** handle 'previous' pokemon 'pokeModal' */
+        if (e.target.dataset.nav === 'prev'){
+            let prev = pokeModal - 1 === -1 ? pokemon.length - 1 : pokeModal - 1;
+            setPokeModal(prev);
+        }
+        /** handle 'next' pokemon 'pokeModal' */
+        if (e.target.dataset.nav === 'next'){
+            let next = pokeModal + 1 === pokemon.length ? 0 : pokeModal + 1;
+            setPokeModal(next);
+        }        
     }
 
     /** return error screen if error has ocurred */
@@ -42,9 +64,12 @@ function Pokedex(){
     return (
         <>
         <div id="overlay" 
-            className={modalShow ? "show" : "hide"}
-            onClick={toggleModal}>
-            <Modal {...pokeModal} />
+            className={modalShow ? "show" : "hide"}>
+            <Modal 
+                {...pokemon[pokeModal]} 
+                navigateModal={navigateModal} 
+                toggleModal = {toggleModal}
+            />
         </div>
         <div className="Pokedex">
         <ol id="pokedex">
