17d59d14310993e75bf37b04bb3eaafa5be42e6c
kevinreber <kevinreber1@gmail.com>
2020-07-03 18:24:19 -0700

    Updates

---
+++ precache-manifest.98b4fcd86b5bec1db3637e4ca2db8f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5fd6ede6c2b01cf977f95b33f389eb",
+    "url": "/PokeDex/index.html"
+  },
+  {
+    "revision": "9e0ad81d0ed38153aeff",
+    "url": "/PokeDex/static/css/main.a9433049.chunk.css"
+  },
+  {
+    "revision": "ce0609c8a793202b11dc",
+    "url": "/PokeDex/static/js/2.0604d4ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokeDex/static/js/2.0604d4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0ad81d0ed38153aeff",
+    "url": "/PokeDex/static/js/main.ef6abdc0.chunk.js"
+  },
+  {
+    "revision": "0ee1a4ba45b24eadbcc3",
+    "url": "/PokeDex/static/js/runtime-main.12f5fbb2.js"
+  },
+  {
+    "revision": "4b41f8bbd0b4aaf0f1cb4e643df6a89c",
+    "url": "/PokeDex/static/media/pokeball.4b41f8bb.png"
+  }
+]);
